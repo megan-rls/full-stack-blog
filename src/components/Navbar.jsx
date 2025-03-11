65911f5d8a97a06c3c766f1c7b3734aeeaab941e
meganl1e <m3ganl1e@gmail.com>
2025-03-11 16:17:55 -0700

    initial commit
    changed body css, added navbar with logo on right

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+const Navbar = () => {
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img src="/logo.png" className="w-8 h-8" alt="" />
+        <span>lamalog</span>
+      </div>
+      {/* MOBILE MENU */}
+      <div className="md: hidden">M</div>
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex">D</div>
+    </div>
+  )
+}
+
+export default Navbar

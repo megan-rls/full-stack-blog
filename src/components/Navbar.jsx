ccc94fbd86692f31de57f98d7b1c146223caa0e8
meganl1e <m3ganl1e@gmail.com>
2025-03-11 17:35:03 -0700

    navbar and hamburger
    links are put for navbar and hamburger, also styled now

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react"
+
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
+
+  const handleClick = () => {
+    setOpen((prev) => !prev)
+  }
+
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* LOGO */}
@@ -6,10 +15,35 @@ const Navbar = () => {
         <img src="/logo.png" className="w-8 h-8" alt="" />
         <span>lamalog</span>
       </div>
+
       {/* MOBILE MENU */}
-      <div className="md: hidden">M</div>
+      <div className="md:hidden">
+        {/* MOBILE MENU BUTTON */}
+        <div className="cursor-pointer text-4xl" onClick={handleClick}>{open ? "X" : "☰"}</div>
+        {/* MOBILE LINK LIST */}
+        <div className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+          open ? "-right-0" : "-right-[100%]"
+        }`}>
+          <a href="/">Home</a>
+          <a href="/">Trending</a>
+          <a href="/">Most Popular</a>
+          <a href="/">About</a>
+          <a href="/">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
+          </a>
+        </div>
+      </div>
+
       {/* DESKTOP MENU */}
-      <div className="hidden md:flex">D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <a href="/">Home</a>
+        <a href="/">Trending</a>
+        <a href="/">Most Popular</a>
+        <a href="/">About</a>
+        <a href="/">
+          <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">Login 👋</button>
+        </a>
+      </div>
     </div>
   )
 }

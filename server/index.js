ccef826a8d6045a2160086d4ad7e82bccea18fc3
meganl1e <m3ganl1e@gmail.com>
2025-03-14 15:13:22 -0700

    backend creation
    backend has been created, using express, separated frontend and backend into different folders

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import userRouter from "./routes/user.route.js"
+import postRouter from "./routes/post.route.js"
+import commentRouter from "./routes/comment.route.js"
+
+const app = express()
+
+// app.get("/test", (req,res) => {
+//   res.status(200).send("it works")
+// })
+
+app.use("/users", userRouter);
+app.use("/posts", postRouter);
+app.use("/comments", commentRouter);
+
+app.listen(3000, () => {
+  console.log("server is running")
+})

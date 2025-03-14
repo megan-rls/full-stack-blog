15a1428818e38a06c2819fcec750c2e129de0475
meganl1e <m3ganl1e@gmail.com>
2025-03-14 14:19:52 -0700

    posts list page
    posts list page with side menu

--- src/routes/PostListPage.jsx
+++ src/routes/PostListPage.jsx
@@ -1,6 +1,28 @@
+import PostList from "../components/PostList";
+import SideMenu from "../components/SideMenu";
+import { useState } from "react";
+
 const PostListPage = () => {
+
+  const [open,setOpen] = useState(false)
+
   return (
-    <div className=''></div>
+    <div className=''>
+      <h1 className="mb-8 text-2xl">Development Blog</h1>
+      <button onClick={()=>setOpen(prev=>!prev)} className="text-white md:hidden bg-blue-800 text-sm px-4 py-2 rounded-2xl mb-4">
+        {open ? "Close" : "Filter or Search"}
+      </button>
+      <div className="flex flex-col-reverse gap-8 md:flex-row">
+        <div>
+          <PostList />
+        </div>
+        {/* on medium screens and above, side menu will always appear */}
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />
+        </div>
+
+      </div>
+    </div>
   )
 }
 
